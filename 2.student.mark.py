07eb565cf66c1484b04eaf7928aacc3b21ba3423
khainguyen2820-ux <khainguyen2820@gmail.com>
2025-12-13 16:51:03 +0700

    charge 1

--- 2.student.mark.py
+++ 2.student.mark.py
@@ -45,53 +45,54 @@ class Course:
     def __course_str__(self):
         return f"{self.__course_name} id: {self.__course_id}"
 
-class University:
+class Mark_for_Student:
     def __init__(self):
         self.__student = {}
         self.__course = {}
         self.__mark = {}
 
     def add_student(self):
-        student_name = input("enter name of student")
-        student_id  = input("enter id of student")
-        DoB = input("enter DoB")
+        student_name = input("enter name of student: ")
+        student_id  = input("enter id of studen: ")
+        while student_id in self.__student.keys():
+            student_id = input("this student id already exist, enter new student id: ")
+        DoB = input("enter student's DoB: ")
         student = Student(student_id, student_name, DoB)
-        self.student[student_id] = student
+        self.__student[student_id] = student
     
     def add_course(self):
-        course_name = input("enter course name")
-        course_id = input("enter course id")
-        course = Course(course_name, course_id)
-        self.course[course_id] = course
+        course_name = input("enter course name: ")
+        while course_name in self.__course.values():
+            course_name = input("already have this course, enter course name: ")
+        course_id = input("enter course id: ")
+        while course_id in self.__course.keys():
+            course_id = input("already have this id, enter course id: ")
+        course_credit = int(input("enter number of credit: "))
+        course = Course(course_name, course_id, course_credit)
+        self.__course[course_id] = course
 
     def set_mark(self):
-        course_id = input("enter course id to input mark")
-        if course_id not in self.course:
-            print("course not found")
-            return
-        
-        student_id = input("enter student id to input mark")
-        if student_id not in self.student:
-            print("student not found")
-            return
-        
-        mark = input("input mark:")
-        self.mark[course_id][student_id] = mark
+        for course_id in self.__course.keys():
+            for student_id in self.__student.keys():
+                mark = int(input(f"Enter mark of course {self.__course[course_id]} for student {self.__student[student_id]}: "))
+                self.__mark[student_id][course_id] = mark
 
     def show_data(self):
         print("\n Student:")
         for student in self.__student.values():
             print(student)
+
         print("\n Course:")
         for course in self.__course.values():
             print(course)
-        print("\n Marks:")
-        for cid, mark_data in self.__marks.items():
-            print(f"Course: {self.__course[cid].getCName()}")
-            for sid, mark in mark_data.items():
-                print(f"  Student {self.__studens[sid].getSName()}: {mark}")
 
-system = University()
+        print("\n Marks:")
+        for student_id in self.__marks.items():
+            print(f"Student id {self.__student[student_id].getSID}: \n")
+            for course_id in self.__mark.item():
+                print(f"Course id {self.__course[course_id].getCID} mark: {self.__mark[student_id][course_id]} \n")
+
+system = Mark_for_Student()
 
 NumOfStudents = int(input("Enter number of students: "))
 for _ in range(NumOfStudents):
@@ -101,5 +102,5 @@ NumOfCourses = int(input("Enter number of courses: "))
 for _ in range(NumOfCourses):
     system.add_course()
 
-system.input_mark()
+system.set_mark()
 system.show_data()
